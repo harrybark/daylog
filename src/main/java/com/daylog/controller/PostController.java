d6fe1f6636245062082c36df22a2c8f6dfb52cb7
harrybark <harrypmw@gmail.com>
2023-07-02 23:19:34 +0900

    feat. init project

---
+++ src/main/java/com/daylog/controller/PostController.java
@@ -0,0 +1,13 @@
+package com.daylog.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PostController {
+
+    // SSR -> JSP, Thymeleaf, mustache, freemarker
+    /* SPA
+     -> vue + SSR = nuxt.js
+     -> react + SSR = next.js
+    */
+}
