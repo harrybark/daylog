8b20e79ed3037b0985b60e94de8b703a145c2adb
harrybark <harrypmw@gmail.com>
2023-07-02 23:29:26 +0900

    feat. Add PostController & Test Class

--- src/test/java/com/daylog/controller/PostControllerTest.java
+++ src/test/java/com/daylog/controller/PostControllerTest.java
@@ -9,6 +9,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
@@ -28,6 +29,7 @@ class PostControllerTest {
         mockMvc.perform(MockMvcRequestBuilders.get("/posts"))
                 .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.content().string("Hello World"))
+                .andDo(print())
         ;
     }
 }
