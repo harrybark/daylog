a43a330ac870b4b8a68c5c778b8473de09d32761
harrybark <harrypmw@gmail.com>
2023-07-05 23:54:25 +0900

    feat.Add ControllerAdvice

--- src/main/java/com/daylog/controller/PostController.java
+++ src/main/java/com/daylog/controller/PostController.java
@@ -6,22 +6,18 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.util.List;
 
 @Slf4j
 @RestController
 public class PostController {
 
     @PostMapping("/posts")
-    public ResponseEntity<?> post(@Valid @RequestBody PostCreate postCreate, BindingResult bindingResult) {
+    public ResponseEntity<?> post(@Valid @RequestBody PostCreate postCreate) {
         log.info("PostCreate = {}", postCreate);
 
         return new ResponseEntity<>(new CMRespDto<>(1, "success", null), HttpStatus.CREATED);
