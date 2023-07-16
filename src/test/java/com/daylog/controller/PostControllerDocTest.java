d6ecd1379803b45a030cffba6445db7ed848da42
harrybark <harrypmw@gmail.com>
2023-07-16 23:33:31 +0900

    feat.Rest Doc

--- src/test/java/com/daylog/controller/PostControllerDocTest.java
+++ src/test/java/com/daylog/controller/PostControllerDocTest.java
@@ -2,41 +2,41 @@ package com.daylog.controller;
 
 import com.daylog.request.PostCreate;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
+import org.springframework.restdocs.payload.PayloadDocumentation;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs(uriScheme = "https", uriHost = "api.daylog.com", uriPort = 443)
 @ExtendWith(RestDocumentationExtension.class)
 public class PostControllerDocTest {
 
+    @Autowired
     private MockMvc mockMvc;
 
-    @BeforeEach
-    void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
-                .apply(documentationConfiguration(restDocumentation))
-                .build();
-
-
-    }
-
+    @Autowired
+    private ObjectMapper objectMapper;
     @Test
     @DisplayName(value = "글 단건 조회 테스트")
     public void 글_단건_조회() throws Exception {
@@ -60,7 +60,19 @@ public class PostControllerDocTest {
         this.mockMvc.perform(get("/posts/{postId}", 1L).accept(APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andDo(document("index"))
+                .andDo(document("index",
+                            pathParameters(
+                                parameterWithName("postId").description("게시글 ID")
+                                    ),
+                            responseFields(
+                                    fieldWithPath("code").description("Result Code"),
+                                    fieldWithPath("message").description("Result Message"),
+                                    fieldWithPath("data.id").description("게시글 ID"),
+                                    fieldWithPath("data.title").description("게시글 제목"),
+                                    fieldWithPath("data.contents").description("게시글 내용")
+                            )
+                        )
+                )
                 ;
 
         // when
@@ -69,4 +81,43 @@ public class PostControllerDocTest {
 
     }
 
+    @Test
+    @DisplayName(value = "글 등록 테스트")
+    public void 글_등록() throws Exception {
+        // given
+        String title = "Apple";
+        String contents = "Prologue";
+        PostCreate postCreate = PostCreate.builder()
+                .title(title)
+                .contents(contents)
+                .build();
+
+        String json = objectMapper.writeValueAsString(postCreate);
+
+        // when
+        mockMvc.perform(post("/posts")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(json)
+                )
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andDo(document("index",
+                                requestFields(
+                                        fieldWithPath("title").description("게시글 제목"),
+                                        fieldWithPath("contents").description("게시글 내용")
+                                ),
+                                responseFields(
+                                        fieldWithPath("code").description("Result Code"),
+                                        fieldWithPath("message").description("Result Message")
+                                )
+                        )
+                )
+        ;
+
+
+        // when
+
+        // then
+
+    }
 }
