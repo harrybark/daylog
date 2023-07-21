53e19c92a7c0b039b8d463c3c00c07cd8170a895
harrybark <harrypmw@gmail.com>
2023-07-21 22:39:12 +0900

    feat.create Read 1 Post page

---
+++ front/src/views/ReadView.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import {defineProps, onMounted, ref} from "vue";
+import axios from "axios";
+
+const props = defineProps({
+  postId : {
+    type : [Number, String],
+    required: true
+  }
+});
+
+const post = ref({
+  id: 0,
+  title: "",
+  contents: ""
+});
+
+onMounted(() => {
+  axios.get(`/api/posts/${props.postId}`).then((response) => {
+    post.value = response.data.data;
+  });
+})
+
+</script>
+
+<template>
+  <h2>{{ post.title }}</h2>
+  <div>{{ post.contents }}</div>
+</template>
