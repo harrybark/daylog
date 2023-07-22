32a6f65cc9f90c5f1f408d0db99e05e7dbaa22f4
harrybark <harrypmw@gmail.com>
2023-07-22 22:22:05 +0900

    feat. add Comment

--- src/main/java/com/daylog/config/WebConfig.java
+++ src/main/java/com/daylog/config/WebConfig.java
@@ -4,8 +4,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    
+    /***
+     * view요청을 통과시키기 위함
+     */
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
