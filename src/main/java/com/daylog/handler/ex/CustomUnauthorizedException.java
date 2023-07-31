f7178d821e0c389781e4b6280e0eb837d14a8787
harrybark <devharypmw@gmail.com>
2023-07-31 22:00:42 +0900

    feat.Add Exception for UnAuthorization

--- src/main/java/com/daylog/handler/ex/CustomUnauthorizedException.java
+++ src/main/java/com/daylog/handler/ex/CustomUnauthorizedException.java
@@ -1,2 +1,20 @@
-package com.daylog.handler.ex;public class CustomUnauthorizedException {
+package com.daylog.handler.ex;
+
+import org.springframework.http.HttpStatus;
+
+public class CustomUnauthorizedException extends CustomCommonException {
+
+    // 객체를 구분할 때
+    private static final long serialVersionUID = 1L;
+
+    private static final String MESSAGE = "Post Not Found";
+
+    public CustomUnauthorizedException() {
+        super(MESSAGE);
+    }
+
+    @Override
+    public HttpStatus statusCode() {
+        return HttpStatus.UNAUTHORIZED;
+    }
 }
