3510ecc7c97ec9df07552a81473a2f12d6729b20
harrybark <harrypmw@gmail.com>
2023-07-31 22:54:27 +0900

    feat.Edit AuthResolver, Adding Autorization

--- src/main/java/com/daylog/config/AuthResolver.java
+++ src/main/java/com/daylog/config/AuthResolver.java
@@ -1,5 +1,6 @@
 package com.daylog.config;
 
+import com.daylog.handler.ex.CustomUnAuthorizedException;
 import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
@@ -16,8 +17,13 @@ public class AuthResolver implements HandlerMethodArgumentResolver {
 
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        String accessToken = webRequest.getParameter("accessToken");
+        if ( accessToken == null || "".equals(accessToken)) {
+            throw new CustomUnAuthorizedException();
+        }
+
         UserSession userSession = new UserSession();
-        userSession.name = "greytomato";
+        userSession.name = accessToken;
         return userSession;
     }
 }
