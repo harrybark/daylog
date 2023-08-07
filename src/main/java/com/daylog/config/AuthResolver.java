1adf26b2db47c669997b16eed7b2af6e10aaf35d
harrybark <harrypmw@gmail.com>
2023-08-07 23:56:38 +0900

    Feat. JWT를 사용하여, AuthResolver, AuthController, AuthService를 변경함

--- src/main/java/com/daylog/config/AuthResolver.java
+++ src/main/java/com/daylog/config/AuthResolver.java
@@ -1,19 +1,27 @@
 package com.daylog.config;
 
 import com.daylog.handler.ex.CustomUnAuthorizedException;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import java.util.Arrays;
+import static org.apache.tomcat.util.codec.binary.Base64.*;
+
 
 @Slf4j
+@RequiredArgsConstructor
 public class AuthResolver implements HandlerMethodArgumentResolver {
+
+    private final String KEY = "v8up323RprJyfvrYvGCHYZG5jq3vgFg7tkhaJoQhY6U=";
 
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
@@ -24,6 +32,7 @@ public class AuthResolver implements HandlerMethodArgumentResolver {
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
         //String accessToken = webRequest.getHeader("Authorization");
+        /*
         HttpServletRequest httpServletRequest = webRequest.getNativeRequest(HttpServletRequest.class);
 
         if ( httpServletRequest == null ) {
@@ -44,9 +53,25 @@ public class AuthResolver implements HandlerMethodArgumentResolver {
         if ( accessToken == null || "".equals(accessToken)) {
             throw new CustomUnAuthorizedException();
         }
+        */
 
-        UserSession userSession = new UserSession(1L);
+        String jws = webRequest.getHeader("Authorization");
+        if ( jws == null || jws.equals("") ) {
+            throw new CustomUnAuthorizedException();
+        }
 
-        return userSession;
+        try {
+
+            Jws<Claims> claimsJws = Jwts.parserBuilder()
+                    .setSigningKey(decodeBase64(KEY))
+                    .build()
+                    .parseClaimsJws(jws);
+
+            String userId = claimsJws.getBody().getSubject();
+            return new UserSession(Long.parseLong(userId));
+
+        } catch (JwtException e) {
+            throw new CustomUnAuthorizedException();
+        }
     }
 }
--- src/main/java/com/daylog/controller/AuthController.java
+++ src/main/java/com/daylog/controller/AuthController.java
@@ -2,18 +2,21 @@ package com.daylog.controller;
 
 import com.daylog.common.CMRespDto;
 import com.daylog.request.LoginRequest;
+import com.daylog.response.SessionResponse;
 import com.daylog.service.AuthService;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.Duration;
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.util.Base64;
 
 @Slf4j
 @RestController
@@ -22,11 +25,21 @@ public class AuthController {
 
     private final AuthService authService;
 
+    private final String KEY = "v8up323RprJyfvrYvGCHYZG5jq3vgFg7tkhaJoQhY6U=";
     @PostMapping("/auth/login")
     public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
         log.info("login {}", loginRequest);
-        String accessToken = authService.signIn(loginRequest);
+        Long userId = authService.signIn(loginRequest);
 
+        Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+        SecretKey secretKey = Keys.hmacShaKeyFor(Base64.getDecoder().decode(KEY));
+
+        String jws = Jwts.builder()
+                .setSubject(String.valueOf(userId))
+                .signWith(secretKey)
+                .compact();
+
+        /*
         ResponseCookie responseCookie = ResponseCookie.from("SESSION", accessToken)
                 .domain("localhost") // todo 서버 환경에 따른 분리 필요!
                 .path("/")
@@ -35,10 +48,11 @@ public class AuthController {
                 .maxAge(Duration.ofDays(30))
                 .sameSite("Strict")
                 .build();
+        */
 
         return ResponseEntity.ok()
-                .header(HttpHeaders.SET_COOKIE, responseCookie.toString())
-                .body(new CMRespDto<>(1, "success", accessToken))
+                //.header(HttpHeaders.SET_COOKIE, responseCookie.toString())
+                .body(new CMRespDto<>(1, "success", new SessionResponse(jws)))
                 ;
 
     }
